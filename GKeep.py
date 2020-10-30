44a113fb78b9cd6cd7dfa63ccd645070451ffaf3
Deividas Ovsianikovas <deividov2000@gmail.com>
2020-10-30 02:28:40 +0000

    Comments & Loops

--- GKeep.py
+++ GKeep.py
@@ -1,3 +1,4 @@
+#All imports
 import gkeepapi
 import xlwt
 import xlsxwriter
@@ -6,30 +7,22 @@ from xlrd import open_workbook
 from datetime import datetime
 from datetime import date
 
-pathToFile = r'C:\Users\Deivid\Desktop\test.xls'
+pathToFile = r''
 
 keep = gkeepapi.Keep()
 #!Important...make sure to delete login credentials....
-success = keep.login('<email>','<password>')
+success = keep.login('','')
 
+#Set variable note equal to note ID
 
-#note = keep.createList('ToDo',[('Item 1', False), ('Item 2', False), ('Item 3', True)])
-note = keep.get('<noteID>')
+gnotes = keep.all()
 
-keep.sync()
-
-print (note.title)
-print (note.text)
-
-
-style0 = xlwt.easyxf('font: name Times New Roman, color-index red, bold on',
-    num_format_str='#,##0.00')
 style1 = xlwt.easyxf(num_format_str='D-MMM-YY')
-
 
 rb = open_workbook(pathToFile)
 r_sheet = rb.sheet_by_index(0)
 
+#Copy current sheet values to variable
 wb = copy(rb)
 ws = wb.get_sheet(0)
 
@@ -39,32 +32,46 @@ workbook = xlsxwriter.Workbook('chart_line.xlsx')
 worksheet = workbook.add_worksheet()
 chart = workbook.add_chart({'type': 'line'})
 
-#cols, rows, value
-ws.write(0, 0, note.title)
-ws.write(1, 0, datetime.now(), style1)
-
-i = r_sheet.nrows
-j = 0
+totalRows = r_sheet.nrows
+columns = 0
 total = 0
 completed = 0
 
 today = date.today()
-ws.write(i+1,0,today.strftime("%B %d, %Y"))
+ws.write(totalRows+1,0,today.strftime("%B %d, %Y"))
 
-for x in note.items:
-    if j < len(note.items):
-        ws.write(i+2, 0,  note.items[j].text)
-        ws.write(i+2, 1, note.items[j].checked)
-
-        if x.checked:
-            completed+=1
-    j+=1
-    i+=1
+#loop sorting list based on when notes where added
 
 
-total =  len(note.items)
+#Loops going through all notes and all boxes within the gKeep notes
+for y in gnotes:
+    #write to excel(cols, rows, value, what style)
+    ws.write(0, 0, y.title)
+    ws.write(1, 0, datetime.now(), style1)
+    #print note values
+    print (y.title)
+    print (y.text)
 
-ws.write(i+3, 0, 'Total Completed:')
-ws.write(i+3, 1,  str(completed) + '/' + str(total))
+    for x in y.items:
+            ws.write(totalRows+2, 0,  x.text)
+            ws.write(totalRows+2, 1, x.checked)  
+
+            if x.checked:
+                completed+=1
+            
+            x.checked = False  
+            totalRows+=1 
+
+    total =  len(y.items)
+
+    #Write values to the worksheet
+    ws.write(totalRows+3, columns, 'Total Completed:')
+    ws.write(totalRows+3, columns+1,  str(completed) + '/' + str(total))
+    columns += 2
+
+    #end of loop
 
 wb.save('test.xls')
+
+#Save Changes
+keep.sync()
