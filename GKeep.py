cfc335d013b6dd7175404cbb4ea10ae8afbe2b2a
Deividas Ovsianikovas <deividov2000@gmail.com>
2020-11-20 12:59:32 +0000

    Columns added for every row, better readability

--- GKeep.py
+++ GKeep.py
@@ -1,3 +1,6 @@
+#TO Do
+#Make sure rows shift down with every update
+
 #All imports
 import gkeepapi
 import xlwt
@@ -7,16 +10,19 @@ from xlrd import open_workbook
 from datetime import datetime
 from datetime import date
 
-pathToFile = r''
+pathToFile = r'D:\GoogleKeepPython\Python-Google-Keep-API-\test.xls'
 
 keep = gkeepapi.Keep()
+
 #!Important...make sure to delete login credentials....
-success = keep.login('','')
+success = keep.login('<Email>','<Password>!')
 
 #Set variable note equal to note ID
 
 gnotes = keep.all()
 
+
+#---Setting up the Excel Sheet------
 style1 = xlwt.easyxf(num_format_str='D-MMM-YY')
 
 rb = open_workbook(pathToFile)
@@ -26,35 +32,44 @@ r_sheet = rb.sheet_by_index(0)
 wb = copy(rb)
 ws = wb.get_sheet(0)
 
-
 #Only works with XLSWriter, need to find a way to set up automatically
 workbook = xlsxwriter.Workbook('chart_line.xlsx')
 worksheet = workbook.add_worksheet()
 chart = workbook.add_chart({'type': 'line'})
 
-totalRows = r_sheet.nrows
+totalRows = r_sheet.nrows #All existing rows in sheet
+#----End Excel Sheet setup--------
+
+
+#Init varialbes
 columns = 0
-total = 0
-completed = 0
+total = 0 #Total num of tasks to be done
+completed = 0 #Actual num of completed tasks today
+maxCheckLists = 0
 
-today = date.today()
-ws.write(totalRows+1,0,today.strftime("%B %d, %Y"))
+#Get the max number of items in any note
+for y in gnotes:
+    if maxCheckLists < len(y.items):
+        maxCheckLists = len(y.items)
 
-#loop sorting list based on when notes where added
-
-
+#Shift all existing columns down based on the maximum items
+for x in range(totalRows):
+    worksheet.set_row(x, 100) 
+    
 #Loops going through all notes and all boxes within the gKeep notes
 for y in gnotes:
+
+    totalRows = 0
     #write to excel(cols, rows, value, what style)
-    ws.write(0, 0, y.title)
-    ws.write(1, 0, datetime.now(), style1)
+    ws.write(0, columns, y.title)
+    ws.write(1, columns, datetime.now(), style1)
     #print note values
     print (y.title)
     print (y.text)
 
     for x in y.items:
-            ws.write(totalRows+2, 0,  x.text)
-            ws.write(totalRows+2, 1, x.checked)  
+            ws.write(totalRows+2, columns,  x.text)
+            ws.write(totalRows+2, columns+1, x.checked)  
 
             if x.checked:
                 completed+=1
@@ -67,7 +82,9 @@ for y in gnotes:
     #Write values to the worksheet
     ws.write(totalRows+3, columns, 'Total Completed:')
     ws.write(totalRows+3, columns+1,  str(completed) + '/' + str(total))
-    columns += 2
+
+    #2 values for every note + leave a space between each
+    columns += 3
 
     #end of loop
 
