5421aab36484a02824d780f32fd655f7863d88a7
Deividas Ovsianikovas <deividov2000@gmail.com>
2020-12-22 00:25:26 +0000

    OpenPyxl
    Added OpenPyxl, cells shift down to accomodate new entries and code cleanup

--- GKeep.py
+++ GKeep.py
@@ -1,75 +1,96 @@
-#TO Do
-#Make sure rows shift down with every update
 
-#All imports
+
+#Script that returns checkmarks from Google Keep, inputs data to Excel sheet and resets all of the notes to their original state
+
+#@author: Deividas Ovsianikovas
+
+
+#------All imports----------
 import gkeepapi
+from openpyxl.descriptors.base import DateTime
 import xlwt
-import xlsxwriter
+import openpyxl
+from openpyxl import Workbook
 from xlutils.copy import copy
 from xlrd import open_workbook
 from datetime import datetime
 from datetime import date
-
-pathToFile = r'D:\GoogleKeepPython\Python-Google-Keep-API-\test.xls'
-
-keep = gkeepapi.Keep()
-
-#!Important...make sure to delete login credentials....
-success = keep.login('<Email>','<Password>!')
-
-#Set variable note equal to note ID
-
-gnotes = keep.all()
-
-
-#---Setting up the Excel Sheet------
-style1 = xlwt.easyxf(num_format_str='D-MMM-YY')
-
-rb = open_workbook(pathToFile)
-r_sheet = rb.sheet_by_index(0)
-
-#Copy current sheet values to variable
-wb = copy(rb)
-ws = wb.get_sheet(0)
-
-#Only works with XLSWriter, need to find a way to set up automatically
-workbook = xlsxwriter.Workbook('chart_line.xlsx')
-worksheet = workbook.add_worksheet()
-chart = workbook.add_chart({'type': 'line'})
-
-totalRows = r_sheet.nrows #All existing rows in sheet
-#----End Excel Sheet setup--------
+import os
+from os import path
+#------End of imports-------
 
 
 #Init varialbes
-columns = 0
+columns = 1
 total = 0 #Total num of tasks to be done
 completed = 0 #Actual num of completed tasks today
 maxCheckLists = 0
+current_datetime = datetime.now()
+
+WBfilename = 'StaffRecords.xlsx'
+pathToFile = os.getcwd() + '\\' + WBfilename 
+
+
+if(path.exists(pathToFile)):
+    print("Path Exists, continue script")
+else:
+    wb = Workbook()
+    wb.save(filename = WBfilename)
+
+
+#------Google Keep Setup--------
+
+keep = gkeepapi.Keep()
+#!Important...make sure to delete login credentials....
+success = keep.login('<Email>','<Password>')
+gnotes = keep.all()
+
+#------End of Google Keep Setup--------
+
+
+
+
+#---Setting up the Excel Sheet------
+rb = openpyxl.load_workbook(pathToFile) 
+r_sheet = rb.active     
+
+#Copy current sheet values to variable
+wb = rb
+ws = wb.active   
+
+totalRows = ws.max_row #All existing rows in sheet
+#----End Excel Sheet setup--------
+
+
+
+
 
 #Get the max number of items in any note
 for y in gnotes:
     if maxCheckLists < len(y.items):
         maxCheckLists = len(y.items)
 
-#Shift all existing columns down based on the maximum items
-for x in range(totalRows):
-    worksheet.set_row(x, 100) 
+
+
+#Shift all existing columns down based on the maximum items, leave gap of 2
+for x in range(maxCheckLists + 6):
+    ws.insert_rows(0)
     
+
+
 #Loops going through all notes and all boxes within the gKeep notes
 for y in gnotes:
-
-    totalRows = 0
-    #write to excel(cols, rows, value, what style)
-    ws.write(0, columns, y.title)
-    ws.write(1, columns, datetime.now(), style1)
-    #print note values
-    print (y.title)
-    print (y.text)
+    totalRows = 1
+    ws.cell(row = 1, column = columns).value = y.title
+    ws.cell(row = 2, column = columns).value = current_datetime.strftime('%a %y-%m-%d ')
+    
+    #print note values, for debugging
+    #print (y.title)
+    #print (y.text)
 
     for x in y.items:
-            ws.write(totalRows+2, columns,  x.text)
-            ws.write(totalRows+2, columns+1, x.checked)  
+            ws.cell(row = totalRows+2, column = columns).value = x.text
+            ws.cell(row = totalRows+2, column = columns+1).value = x.checked
 
             if x.checked:
                 completed+=1
@@ -78,17 +99,19 @@ for y in gnotes:
             totalRows+=1 
 
     total =  len(y.items)
-
     #Write values to the worksheet
-    ws.write(totalRows+3, columns, 'Total Completed:')
-    ws.write(totalRows+3, columns+1,  str(completed) + '/' + str(total))
+    ws.cell(row = maxCheckLists+4, column = columns).value = "Total Completed: "
+    ws.cell(row = maxCheckLists+4, column = columns+1).value = str(completed) + '/' + str(total)
+    completed = 0
 
-    #2 values for every note + leave a space between each
+    #2 values for every note to leave a space between each
     columns += 3
-
     #end of loop
 
-wb.save('test.xls')
 
-#Save Changes
+
+
+
+#Save Changes in keep and workbook
+wb.save(WBfilename)
 keep.sync()
